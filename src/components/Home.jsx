52e7ae5a6ddbeaf551f6168d661334b33ff56724
half6 <christofferhalf6@gmail.com>
2023-03-07 16:58:37 +0100

    bug fixing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,17 +6,18 @@ export default function Home(){
     const tablet = useMediaQuery('(max-width:800px)');
 
     return(
-    <>
-    <Container   sx={{ display:'flex',flexDirection:'column', justifyContent:'center', marginTop:'100px' }}>
-        <Typography variant={mobile ? 'h4' : tablet ? 'h2' : 'h1'} component='h1' sx={{ textAlign:'center' }} >Welcome to The store </Typography>
-            <Box maxWidth="100vw">
-                <img src='/store_9.jpg'></img>
-            </Box>  
-        
-            <Box sx={{ display:'flex', justifyContent:'center', marginTop:'50px' }}>
-                <Link className='cta' to='/products'>SHOP NOW</Link>   
-            </Box>
-    </Container>
+    <>  
+        <Container   sx={{ display:'flex',flexDirection:'column', justifyContent:'center', marginTop:'100px' }}>
+            <Typography variant={mobile ? 'h4' : tablet ? 'h2' : 'h1'} component='h1' sx={{ textAlign:'center' }} >Welcome to The store </Typography>
+                <Box maxWidth="100vw">
+                    <img src='/store_9.jpg'></img>
+                </Box>  
+
+                <Box sx={{ display:'flex', justifyContent:'center', marginTop:'50px' }}>
+                    <Link className='cta' to='/products'>SHOP NOW</Link>   
+                </Box>
+        </Container>
     </>
     )
 }
+
